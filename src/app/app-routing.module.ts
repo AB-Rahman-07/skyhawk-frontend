4293d54937d4c097f5efa9a85736e17d90e94563
CptPrime69 <mr.mohammad047@outlook.com>
2021-07-31 16:44:01 +0500

    added signup module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: '', pathMatch: 'full', redirectTo: '/landinpage' },
   // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) },
 
   // {path: '', pathMatch: 'full', redirectTo:'/events'},
