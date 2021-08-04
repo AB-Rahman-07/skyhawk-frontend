363bb0e2af85ecc8f9b5f185d43f135126feaa42
CptPrime69 <mr.mohammad047@outlook.com>
2021-08-04 09:48:45 +0500

    fixed footer & NavBar

--- src/app/pages/landingpage/landingpage.module.ts
+++ src/app/pages/landingpage/landingpage.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { LandingpageRoutingModule } from './landingpage-routing.module';
 import { LandingpageComponent } from './landingpage/landingpage.component';
-import { FooterComponent } from '../login/components/footer/footer.component';
-import { NavbarComponent } from '../login/components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [LandingpageComponent, FooterComponent, NavbarComponent],
